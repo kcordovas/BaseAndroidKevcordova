7098ba3717aab293080db0cb723349a787d0c1e7
Kevin Cordova <kevc77@Mac-mini-de-Kevin-C7.local>
2022-05-02 20:05:01 -0500

    first commit

--- app/src/main/java/com/example/baseandroidkevcordova/MainActivity.kt
+++ app/src/main/java/com/example/baseandroidkevcordova/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.baseandroidkevcordova
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.baseandroidkevcordova.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.buttonSharedPreferences.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            binding.buttonSharedPreferences.id -> viewModel.sharedPreferences("Hello SharedPreferences")
+        }
     }
 }
---
+++ app/src/main/java/com/example/baseandroidkevcordova/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.baseandroidkevcordova
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    fun sharedPreferences(valueToSave: String = "ValueToSave") {
+
+    }
+}
